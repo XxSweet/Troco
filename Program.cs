307054f605ead898aedd82fbf9e7b9a9fb6e756f
xxsweet <xxsweet07@gmail.com>
2023-03-31 22:00:20 -0300

    calculo de troco

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿decimal valorCompra, valorPago, valorTroco;
+
+Console.WriteLine("--- Troco MongaShop---\n");
+
+Console.Write("Valor da compra (R$)...: ");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor pago (R$)........: ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+
+valorTroco = valorPago - valorCompra;
+
+Console.Write ("Troco:"); Console.ForegroundColor = ConsoleColor.Green;  Console.WriteLine ($"{valorTroco:C}");
+Console.ResetColor();
